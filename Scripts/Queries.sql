21e926875761eafadea78cecf4d52d53096fc0ed
RaylTzy <160803114+RaylTzy@users.noreply.github.com>
2024-03-25 23:35:14 +0100

    Update Queries.sql

--- Scripts/Queries.sql
+++ Scripts/Queries.sql
@@ -1,7 +1,15 @@
-select p.nameProduct, count(c.id_product) as total_compras
+select p.title, count(1) as total_compras
+from CatalogXUser c
+inner join Product p
+on c.id_product = p.id_product
+group by p.title
+order by count(c.id_product) desc;
+--fetch first 10 rows only;
+
+/*select p.nameProduct, count(c.id_product) as total_compras
 from CatalogXUser c
 inner join Product p
 on c.id_product = p.id_product
 group by nameProduct
 order by count(c.id_product) desc
-fetch first :N rows only;
+fetch first 10 rows only;*/

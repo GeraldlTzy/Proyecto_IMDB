5ef4d6a74fc021207e2152c17142fceedaa65dae
Yousii1 <128955986+Yousii1@users.noreply.github.com>
2024-03-25 22:02:32 -0600

    Add files via upload

---
+++ Scripts/Inserts/Products.sql
@@ -0,0 +1,15 @@
+--Director: Denis villeneuve
+--Actores: Zendaya, Timothée Chalamet
+INSERT INTO Product(id_product,link,price,releaseYear,title,duration,trailer,synopsis,season,episode,photo)
+VALUES (1,"https://es.wikipedia.org/wiki/Dune",4.99,TO_DATE("03/09/2021","DD/MM/YYYY"),"Dune",156,"https://www.youtube.com/watch?v=n9xhJrPXop4&pp=ygUMZHVuZSB0cmFpbGVy","Arrakis, también denominado "Dune", se ha convertido en el planeta más importante del universo. A su alrededor comienza una gigantesca lucha por el poder que culmina en una guerra interestelar.",NULL,NULL,NULL);
+
+--Actores: Jennifer Lawrence, Sam Claflin
+INSERT INTO Product(id_product,link,price,releaseYear,title,duration,trailer,synopsis,season,episode,photo)
+VALUES (2,"https://en.wikipedia.org/wiki/The_Hunger_Games_(film)",4.99,TO_DATE("12/03/2012","DD/MM/YYYY"),"The Hunger Games",142,"https://www.youtube.com/watch?v=PbA63a7H0bo&pp=ygUUaHVuZ2VyIGdhbWVzIHRyYWlsZXI%3D","Para demostrar su poder, el régimen del estado totalitario de Panem organiza cada año 'Los juegos del hambre'. En ellos, 24 jóvenes compiten el uno contra el otro en una batalla en la que solo puede haber un superviviente. La joven Katniss se ofrece voluntaria para participar en los juegos para salvar a su hermana. Junto a ella participará Peeta, un joven al que ha conocido desde la infancia y que está enamorado de ella. Sin embargo, el Capitolio quiere convertirlos en contrincantes.",NULL,NULL,NULL);
+
+--Director: Ari Aster 
+--Actriz: FLorence Pugh
+INSERT INTO Product(id_product,link,price,releaseYear,title,duration,trailer,synopsis,season,episode,photo)
+VALUES (3,"https://es.wikipedia.org/wiki/Midsommar_(pel%C3%ADcula_de_2019)",7.99,TO_DATE("03/07/2019","DD/MM/YYYY"),"Midsommar",147,"https://www.youtube.com/watch?v=fcylOJ0twFw&pp=ygURbWlkc29tbWFyIHRyYWlsZXI%3D","Una pareja de estadounidenses acude con unos amigos a Midsommar, un festival de verano que se celebra cada 90 años en una aldea remota de Suecia. Sin embargo, lo que parecían ser unas vacaciones de ensueño toma un giro macabro cuando los aldeanos los invitan a participar en sus perturbadoras actividades festivas.",NULL,NULL,NULL);
+
+

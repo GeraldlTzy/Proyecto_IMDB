197bc31721dca3a38d1d117692f3bb54862a5823
RaylTzy <160803114+RaylTzy@users.noreply.github.com>
2024-03-27 07:55:48 +0100

    Update Functions.sql

--- Scripts/Functions.sql
+++ Scripts/Functions.sql
@@ -6,61 +6,61 @@
  */
   
 CREATE PROCEDURE insertSex (
-    p_name IN VARCHAR2
+    pName IN VARCHAR2
 ) AS
 BEGIN
-    INSERT INTO sex(id_sex, sex_name)
-    values(s_sex.nextval, p_name);
+    INSERT INTO sex(idSex, sexName)
+    values(s_sex.nextval, pName);
 END insertSex;
 
 CREATE PROCEDURE insertNationality(
-    p_name IN VARCHAR2
+    pName IN VARCHAR2
 ) AS
 BEGIN
-    INSERT INTO nationality (id_nationality, name)
-    VALUES (s_nationality.nextval, p_name);
+    INSERT INTO nationality (idNationality, name)
+    VALUES (s_nationality.nextval, pName);
     COMMIT;
 END insertNationality;
 
 CREATE PROCEDURE insertPerson(
-    p_sex IN NUMBER,
-    p_first_name IN VARCHAR2,
-    p_second_name IN VARCHAR2,
-    p_first_surname IN VARCHAR2,
-    p_second_surname IN VARCHAR2,
-    p_date_birth IN DATE
+    pSex IN NUMBER,
+    pFirstName IN VARCHAR2,
+    pSecondName IN VARCHAR2,
+    pFirstSurname IN VARCHAR2,
+    pSecondSurname IN VARCHAR2,
+    pDatebirth IN DATE
 ) AS
 BEGIN
-    INSERT INTO Person (id_person, id_sex, first_name, second_name, first_surname,
-    second_surname, date_birth)
-    VALUES (s_person.nextval, p_sex, p_first_name, p_second_name, p_first_surname,
-    p_second_surname, p_date_birth);
+    INSERT INTO Person (idPerson, idSex, firstName, secondName, firstSurname,
+    secondSurname, datebirth)
+    VALUES (s_person.nextval, pSex, pFirstName, pSecondName, pFirstSurname,
+    pSecondSurname, pDatebirth);
     COMMIT;
 END insertPerson;
 
 CREATE PROCEDURE insertParticipant(
-    p_sex IN NUMBER,
-    p_first_name IN VARCHAR2,
-    p_second_name IN VARCHAR2,
-    p_first_surname IN VARCHAR2,
-    p_second_surname IN VARCHAR2,
-    p_date_birth IN DATE,
-    p_country IN NUMBER,
-    p_biography IN VARCHAR2,
-    p_height IN NUMBER,
-    p_trivia IN VARCHAR2,
-    p_photo IN BLOB
+    pSex IN NUMBER,
+    pFirstName IN VARCHAR2,
+    pSecondName IN VARCHAR2,
+    pFirstSurname IN VARCHAR2,
+    pSecondSurname IN VARCHAR2,
+    pDateBirth IN DATE,
+    pCountry IN NUMBER,
+    pBiography IN VARCHAR2,
+    pHeight IN NUMBER,
+    pTrivia IN VARCHAR2,
+    pPhoto IN BLOB
 ) AS
 BEGIN
-    INSERT INTO Person (id_person, id_sex, first_name, second_name, first_surname,
-    second_surname, date_birth)
-    VALUES (s_person.nextval, p_sex, p_first_name, p_second_name, p_first_surname,
-    p_second_surname, p_date_birth);
+    INSERT INTO Person (idPerson, idSex, firstName, secondName, firstSurname,
+    secondSurname, datebirth)
+    VALUES (s_person.nextval, pSex, pFirstName, pSecondName, pFirstSurname,
+    pSecondSurname, pDatebirth);
     
-    INSERT INTO Participant (id_participant, id_country, biography, height, trivia,
+    INSERT INTO Participant (idParticipant, idCountry, biography, height, trivia,
     photo)
-    values(s_person.currval, p_country, p_biography, p_height, p_trivia,
-    p_photo);
+    values(s_person.currval, pCountry, pBiography, pHeight, pTrivia,
+    pPhoto);
     
     COMMIT;
 END insertParticipant;
@@ -77,62 +77,62 @@ END;*/
 
 CREATE PROCEDURE insertSystemUser(
     /*Person atributes*/
-    p_sex IN NUMBER,
-    p_first_name IN VARCHAR2,
-    p_second_name IN VARCHAR2,
-    p_first_surname IN VARCHAR2,
-    p_second_surname IN VARCHAR2,
-    p_date_birth IN DATE,
+    pSex IN NUMBER,
+    pFirstName IN VARCHAR2,
+    pSecondName IN VARCHAR2,
+    pFirstSurname IN VARCHAR2,
+    pSecondSurname IN VARCHAR2,
+    pDatebirth IN DATE,
     /*SystemUser atributes*/
-    p_username IN VARCHAR2,
-    p_identification IN VARCHAR2,
-    p_phoneNumber IN NUMBER,
-    p_email IN VARCHAR2,
-    p_pswd IN VARCHAR2
+    pUsername IN VARCHAR2,
+    pIdentification IN VARCHAR2,
+    pPhoneNumber IN NUMBER,
+    pEmail IN VARCHAR2,
+    pPswd IN VARCHAR2
 ) AS
 BEGIN
-    INSERT INTO Person (id_person, id_sex, first_name, second_name, first_surname,
-    second_surname, date_birth)
-    VALUES (s_person.nextval, p_sex, p_first_name, p_second_name, p_first_surname,
-    p_second_surname, p_date_birth);
+    INSERT INTO Person (idPerson, idSex, firstName, secondName, firstSurname,
+    secondSurname, datebirth)
+    VALUES (s_person.nextval, pSex, pFirstName, pSecondName, pFirstSurname,
+    pSecondSurname, pDatebirth);
     
-    INSERT INTO systemUser (id_systemUser, username, identification, phoneNumber,
+    INSERT INTO systemUser (idSystemUser, username, identification, phoneNumber,
     email, pswd)
-    VALUES (s_person.currval, p_username, p_identification, p_phoneNumber, p_email,
-    p_pswd);
+    VALUES (s_person.currval, pUsername, pIdentification, pPhoneNumber, pEmail,
+    pPswd);
     
     COMMIT;
 END insertSystemUser;
 
 CREATE PROCEDURE insertUser(
     /*Person atributes*/
-    p_sex IN NUMBER,
-    p_first_name IN VARCHAR2,
-    p_second_name IN VARCHAR2,
-    p_first_surname IN VARCHAR2,
-    p_second_surname IN VARCHAR2,
-    p_date_birth IN DATE,
+    pSex IN NUMBER,
+    pFirstName IN VARCHAR2,
+    pSecondName IN VARCHAR2,
+    pFirstSurname IN VARCHAR2,
+    pSecondSurname IN VARCHAR2,
+    pDatebirth IN DATE,
     /*SystemUser atributes*/
-    p_username IN VARCHAR2,
-    p_identification IN VARCHAR2,
-    p_phoneNumber IN NUMBER,
-    p_email IN VARCHAR2,
-    p_password IN VARCHAR2
+    pUsername IN VARCHAR2,
+    pIdentification IN VARCHAR2,
+    pPhoneNumber IN NUMBER,
+    pEmail IN VARCHAR2,
+    pPswd IN VARCHAR2
     /*end_user atributes*/
     ---Only id
 ) AS
 BEGIN
-    INSERT INTO Person (id_person, id_sex, first_name, second_name, first_surname,
-    second_surname, date_birth)
-    VALUES (s_person.nextval, p_sex, p_first_name, p_second_name, p_first_surname,
-    p_second_surname, p_date_birth);
+    INSERT INTO Person (idPerson, idSex, firstName, secondName, firstSurname,
+    secondSurname, datebirth)
+    VALUES (s_person.nextval, pSex, pFirstName, pSecondName, pFirstSurname,
+    pSecondSurname, pDatebirth);
     
-    INSERT INTO systemUser (id_systemUser, username, identification, phoneNumber,
+    INSERT INTO systemUser (idSystemUser, username, identification, phoneNumber,
     email, pswd)
-    VALUES (s_person.currval, p_username, p_identification, p_phoneNumber, p_email,
-    p_password);
+    VALUES (s_person.currval, pUsername, pIdentification, pPhoneNumber, pEmail,
+    pPswd);
     
-    INSERT INTO end_user (id_user)
+    INSERT INTO end_user (idUser)
     VALUES (s_person.currval);
     
     COMMIT;
@@ -140,33 +140,33 @@ END insertUser;
 
 CREATE PROCEDURE insertAdministrator(
     /*Person atributes*/
-    p_sex IN NUMBER,
-    p_first_name IN VARCHAR2,
-    p_second_name IN VARCHAR2,
-    p_first_surname IN VARCHAR2,
-    p_second_surname IN VARCHAR2,
-    p_date_birth IN DATE,
+    pSex IN NUMBER,
+    pFirstName IN VARCHAR2,
+    pSecondName IN VARCHAR2,
+    pFirstSurname IN VARCHAR2,
+    pSecondSurname IN VARCHAR2,
+    pDatebirth IN DATE,
     /*SystemUser atributes*/
-    p_username IN VARCHAR2,
-    p_identification IN VARCHAR2,
-    p_phoneNumber IN NUMBER,
-    p_email IN VARCHAR2,
-    p_password IN VARCHAR2
-    /*administrator atributes*/
+    pUsername IN VARCHAR2,
+    pIdentification IN VARCHAR2,
+    pPhoneNumber IN NUMBER,
+    pEmail IN VARCHAR2,
+    pPswd IN VARCHAR2
+    /*Administrator atributes*/
     ---Only id
 ) AS
 BEGIN
-    INSERT INTO Person (id_person, id_sex, first_name, second_name, first_surname,
-    second_surname, date_birth)
-    VALUES (s_person.nextval, p_sex, p_first_name, p_second_name, p_first_surname,
-    p_second_surname, p_date_birth);
+    INSERT INTO Person (idPerson, idSex, firstName, secondName, firstSurname,
+    secondSurname, datebirth)
+    VALUES (s_person.nextval, pSex, pFirstName, pSecondName, pFirstSurname,
+    pSecondSurname, pDatebirth);
     
-    INSERT INTO systemUser (id_systemUser, username, identification, phoneNumber,
+    INSERT INTO systemUser (idSystemUser, username, identification, phoneNumber,
     email, pswd)
-    VALUES (s_person.currval, p_username, p_identification, p_phoneNumber, p_email,
-    p_password);
+    VALUES (s_person.currval, pUsername, pIdentification, pPhoneNumber, pEmail,
+    pPswd);
     
-    INSERT INTO administrator (id_administrator)
+    INSERT INTO administrator (idAdministrator)
     VALUES (s_person.currval);
     
     COMMIT;
@@ -174,18 +174,18 @@ END insertAdministrator;
 
 /******************************************************************************/
 CREATE PROCEDURE insertCard(
-    p_id_user IN NUMBER,
-    p_cardNumber IN NUMBER,
-    p_expiration IN DATE,
-    p_ccv IN NUMBER,
-    p_ownerName IN VARCHAR2
+    pIdUser IN NUMBER,
+    pCardNumber IN NUMBER,
+    pExpiration IN DATE,
+    pCcv IN NUMBER,
+    pOwnerName IN VARCHAR2
 ) AS
 BEGIN
-    INSERT INTO payment (id_payment, id_user)
-    VALUES (s_payment.nextval, p_id_user);
+    INSERT INTO payment (idPayment, idUser)
+    VALUES (s_payment.nextval, pIdUser);
     
-    INSERT INTO card (id_card, cardNumber, expiration, ccv, ownerName)
-    VALUES (s_payment.currval, p_cardNumber, p_expiration, p_ccv, p_ownerName);
+    INSERT INTO card (idCard, cardNumber, expiration, ccv, ownerName)
+    VALUES (s_payment.currval, pCardNumber, pExpiration, pCcv, pOwnerName);
     
     COMMIT;
 END insertCard;
@@ -200,4 +200,4 @@ BEGIN
     INSERT INTO ()
     VALUES ();
     COMMIT;
-END;*/    
+END;*/

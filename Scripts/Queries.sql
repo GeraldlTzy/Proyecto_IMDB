1afe57ab6b492e3fdbaf18e7f57e738a6d885584
Yousii1 <128955986+Yousii1@users.noreply.github.com>
2024-03-31 14:43:41 -0600

    Update Queries.sql

--- Scripts/Queries.sql
+++ Scripts/Queries.sql
@@ -13,3 +13,42 @@ on c.id_product = p.id_product
 group by nameProduct
 order by count(c.id_product) desc
 fetch first 10 rows only;*/
+
+
+--Purchases for 3 months
+SELECT 'Three months', p.title
+FROM User u
+INNER JOIN CatalogXUser cxu
+ON u.id_user = cxu.id_user
+INNER JOIN Product p
+ON cxp.id_product = p.id_product
+WHERE TRUNC((SYSDATE - history)/30) BETWEEN 0 AND 3
+ORDERED BY cxu.history
+
+--Purchases for 6
+SELECT 'Three months', p.title
+FROM User u
+INNER JOIN CatalogXUser cxu
+ON u.id_user = cxu.id_user
+INNER JOIN Product p
+ON cxp.id_product = p.id_product
+WHERE TRUNC((SYSDATE - history)/30) BETWEEN 0 AND 6
+ORDERED BY cxu.history
+
+--Purchases for 12 months
+SELECT 'Three months', p.title
+FROM User u
+INNER JOIN CatalogXUser cxu
+ON u.id_user = cxu.id_user
+INNER JOIN Product p
+ON cxp.id_product = p.id_product
+WHERE TRUNC((SYSDATE - history)/30) BETWEEN 0 AND 12
+ORDERED BY cxu.history
+
+--All purchases
+SELECT 'Three months', p.title
+FROM User u
+INNER JOIN CatalogXUser cxu
+ON u.id_user = cxu.id_user
+INNER JOIN Product p
+ON cxp.id_product = p.id_product

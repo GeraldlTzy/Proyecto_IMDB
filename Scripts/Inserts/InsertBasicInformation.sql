72cb00573120b2ce01988f8500298b5f857aebc2
RaylTzy <160803114+RaylTzy@users.noreply.github.com>
2024-04-27 23:00:16 +0200

    Update InsertBasicInformation.sql

--- Scripts/Inserts/InsertBasicInformation.sql
+++ Scripts/Inserts/InsertBasicInformation.sql
@@ -24,3 +24,13 @@ call pkgBasic.insertTypeParticipant('Productor');
 call pkgBasic.insertTypeParticipant('Guionista');
 call pkgBasic.insertTypeParticipant('Compositor');
 call pkgBasic.insertTypeParticipant('Editor');
+/***************************INSERT ADMINISTRATOR*******************************/
+call pkgAdmin.insertAdministrator(1,'Gerald', 'Estifén', 'Calderón','Castro', TO_DATE('23/06/2004','DD/MM/YYYY'),EMPTY_BLOB(),
+'GECC', 728234493, 95778760, 'gerald@gmail.com', '12345', 1, 69);
+call pkgAdmin.insertAdministrator(1,'José', 'David', 'Serrano','Robles', TO_DATE('08/03/2005', 'DD/MM/YYYY'),EMPTY_BLOB(),
+'JDSR', 202321321, 89437643, 'jose@gmail.com', '54321', 1, 69);
+/***************************INSERT User****************************************/
+call pkgEnd_User.insertUser(1,'Gerald', 'Estifén', 'Calderón','Castro', TO_DATE('23/06/2004','DD/MM/YYYY'),EMPTY_BLOB(),
+'gerald', 728234493, 95778760, 'geraldcalderon@gmail.com', '12345', 1, 69);
+call pkgEnd_User.insertUser(1,'José', 'David', 'Serrano','Robles', TO_DATE('08/03/2005', 'DD/MM/YYYY'),EMPTY_BLOB(),
+'jose', 202321321, 89437643, 'joseserrano@gmail.com', '54321', 1, 69);

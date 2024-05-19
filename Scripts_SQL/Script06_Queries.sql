5f590f3c178f2a83ce1fdad6ff378e066de92976
RaylTzy <160803114+RaylTzy@users.noreply.github.com>
2024-05-20 01:06:38 +0200

    Update Script06_Queries.sql

--- Scripts_SQL/Script06_Queries.sql
+++ Scripts_SQL/Script06_Queries.sql
@@ -1,5 +1,27 @@
-# Queries for users
 DELIMITER //
+# Query for admin
+PROCEDURE getTopNPurchases(IN N int)
+BEGIN
+    SELECT top.idProduct, top.title, ph.image, top.total 
+    FROM
+    Photo ph
+    RIGHT JOIN
+        (
+        SELECT top.idProduct, top.title, top.total
+            FROM (
+                SELECT p.idProduct, p.title, COUNT(c.idProduct) AS total
+                FROM Purchase c
+                INNER JOIN Product p ON c.idProduct = p.idProduct
+                GROUP BY p.idProduct, p.title
+                ORDER BY COUNT(c.idProduct) DESC
+            ) top
+        LIMIT 2
+        ) top
+    ON ph.idProduct = top.idProduct
+    ORDER BY top.total DESC;    
+END;
+//
+# Queries for users
 CREATE OR REPLACE PROCEDURE getPurchasesInLastNMonths(IN itv int)
 BEGIN
 	IF itv IS NULL OR itv = 0 THEN

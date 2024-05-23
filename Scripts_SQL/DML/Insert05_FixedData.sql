526c4bef3fb51fa6e963f4155a032592df113cf8
RaylTzy <160803114+RaylTzy@users.noreply.github.com>
2024-05-23 02:41:25 +0200

    Update and rename fixedData.sql to Insert05_FixedData.sql

--- Scripts_SQL/DML/Insert05_FixedData.sql
+++ Scripts_SQL/DML/Insert05_FixedData.sql
@@ -24,17 +24,9 @@ call insertTypeParticipant('Productor');
 call insertTypeParticipant('Guionista');
 call insertTypeParticipant('Compositor');
 call insertTypeParticipant('Editor');
-/***************************INSERT ADMINISTRATOR*******************************/
-#Falta de cambiar
-/*call insertAdministrator(1,'Gerald', 'Estifén', 'Calderón','Castro', '23/06/2004',EMPTY_BLOB,
-'GECC', 728234493, 95778760, 'gerald@gmail.com', '12345', 1, 69);
-call (1,'José', 'David', 'Serrano','Robles', TO_DATE('08/03/2005', 'DD/MM/YYYY'),EMPTY_BLOB(),
-'JDSR', 202321321, 89437643, 'jose@gmail.com', '54321', 1, 69);
-*/
-/***************************INSERT User****************************************/
-ALTER TABLE systemuser MODIFY COLUMN email VARCHAR(50);
-CALL insertUser(1,'Gerald', 'Estifén', 'Calderón','Castro', '2004-06-23',null,
-'gerald', 728234493, 95778760, 'geraldcalderon@gmail.com', '12345', 1, 69);
-CALL insertUser(1,'José', 'David', 'Serrano','Robles', '2005-03-08', null,
-'jose', 202321321, 89437643, 'joseserrano@gmail.com', '54321', 1, 69);
-
+/******************************************************************************/
+CALL insertCatalog('Acción'); 
+CALL insertCatalog('Comedia');
+CALL insertCatalog('Terror');
+CALL insertCatalog('Romance');
+CALL insertCatalog('Animado');

e1322e9c5b9519b55fd6c226f7d7117aa66cadf7
RaylTzy <160803114+RaylTzy@users.noreply.github.com>
2024-05-29 19:32:00 +0200

    Create Script07_Delete.sql

---
+++ Scripts_SQL/Script07_Delete.sql
@@ -0,0 +1,73 @@
+delimiter //
+CREATE OR REPLACE PROCEDURE deleteCountry(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM country
+	WHERE idCountry = pId;
+	COMMIT; 
+END//
+CREATE OR REPLACE PROCEDURE deleteCity(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM city
+	WHERE idCity = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deleteTypeIdent(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM typeofidentification
+	WHERE idTypeIdent = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deleteCatalog(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM `catalog`
+	WHERE idCatalog = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deleteTypeProduct(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM typeofproduct
+	WHERE idType = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deleteTypeParticipant(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM typeofparticipant
+	WHERE idType = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deletePlatform(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM platform
+	WHERE idPlatform = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deleteSex(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM sex
+	WHERE idSex = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deleteKindship(IN pId INT UNSIGNED)
+BEGIN 
+	DELETE FROM `relative`
+	WHERE idRelative = pId;
+	COMMIT;
+END//
+CREATE OR REPLACE PROCEDURE deleteParticipant(IN pIdParticipant INT UNSIGNED)
+BEGIN
+	START TRANSACTION;
+
+	DELETE FROM ParticipantXRelative
+    WHERE idParticipant = pIdParticipant
+    OR idParticipant2 = pIdParticipant;
+    
+    DELETE FROM ParticipantXProduct
+    WHERE idParticipant = pIdParticipant;
+    
+    DELETE FROM Participant
+    WHERE idParticipant = pIdParticipant;
+
+	COMMIT;
+END;
+//
+delimiter ;

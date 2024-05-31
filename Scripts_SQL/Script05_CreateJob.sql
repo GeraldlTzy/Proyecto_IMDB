92449fed9cd30a70b365df92ab9939781de5a3ce
RaylTzy <160803114+RaylTzy@users.noreply.github.com>
2024-05-31 05:57:24 +0200

    Update Script05_CreateJob.sql

--- Scripts_SQL/Script05_CreateJob.sql
+++ Scripts_SQL/Script05_CreateJob.sql
@@ -17,31 +17,32 @@ INSERT INTO Params(param_name, param) VALUES ('top_products', 10);
 COMMIT;
 SELECT * FROM params;
 
-DELIMITER //
+delimiter //
 CREATE OR REPLACE EVENT top_products_event
-	ON SCHEDULE EVERY 1 DAY
-	STARTS '2024-05-20 00:00:00'
-	DO
-		BEGIN 
+ON SCHEDULE EVERY 1 DAY
+STARTS '2024-05-31 05:54:00.000'
+ON COMPLETION NOT PRESERVE
+ENABLE
+DO BEGIN 
 		DECLARE top_n int;
+		DECLARE query TEXT;
 		DELETE FROM top_products;
 		SELECT param INTO top_n FROM Params WHERE param_name = 'top_products';
-		SET @query = CONCAT(
-			'INSERT INTO top_products (reportDate, idProduct, rating)',
-			'SELECT CURDATE(), p.idProduct, AVG(r.stars) AS avgStars',
-			'FROM product p',
-			'JOIN review r',
-			'ON p.idProduct = r.idProduct',
-			'GROUP BY p.idProduct',
-			'ORDER BY avgStars DESC',
-			'LIMIT ', top_n, ';'
+		SET query = CONCAT(
+			'INSERT INTO top_products (reportDate, idProduct, rating) ',
+			'SELECT CURDATE(), p.idProduct, AVG(r.stars) AS avgStars ',
+			'FROM product p ',
+			'JOIN review r ',
+			'ON p.idProduct = r.idProduct ',
+			'GROUP BY p.idProduct ',
+			'ORDER BY avgStars DESC ',
+			'LIMIT ', 10, ';'
 		);
-		PREPARE stmt FROM @query;
+		PREPARE stmt FROM query;
 		EXECUTE stmt;
 		DEALLOCATE PREPARE stmt;
-		END;
-//
-DELIMITER ;
+		END//
+delimiter ;
 /*SHOW EVENTS;
 DROP EVENT IF EXISTS top_products_event;*/
 /*Reference: https://stackoverflow.com/questions/49282244/create-event-into-mariadb*/
